741a4ddc7bf496da712e5e87b5cbc59f10933d10
Broccolipop <112798043+Broccolipop@users.noreply.github.com>
2022-09-22 16:41:10 +0800

    2022.9.22

---
+++ js/test.js
@@ -0,0 +1,67 @@
+// $(function () {
+//     $('li.test1').click(function () {
+//         $('html,body').scrollTop(660)
+
+//         return false;
+//     });
+// });
+
+
+// 關於電
+$('.test1').click(function () {
+    $('html,body').animate({ scrollTop: $('#test1').offset().top }, 800);
+});
+// 電控制
+$('.test2').click(function () {
+    $('html,body').animate({ scrollTop: $('#test2').offset().top }, 800);
+});
+
+$('.test3').click(function () {
+    $('html,body').animate({ scrollTop: $('#test3').offset().top }, 800);
+});
+
+$('.test4').click(function () {
+    $('html,body').animate({ scrollTop: $('#test4').offset().top }, 800);
+});
+
+$('.test5').click(function () {
+    $('html,body').animate({ scrollTop: $('#test5').offset().top }, 800);
+});
+
+// $(function () {
+//     $('li.test2').click(function () {
+//         $('html,body').scrollTop(1400)
+
+//         return false;
+//     });
+// });
+
+
+
+// $(function () {
+//     $('li.test3').click(function () {
+//         $('html,body').scrollTop(2050)
+
+//         return false;
+//     });
+// });
+
+
+
+// $(function () {
+//     $('li.test4').click(function () {
+//         $('html,body').scrollTop(2800)
+
+//         return false;
+//     });
+// });
+
+
+
+// $(function () {
+//     $('li.test5').click(function () {
+//         $('html,body').scrollTop(3800)
+
+//         return false;
+//     });
+// });
